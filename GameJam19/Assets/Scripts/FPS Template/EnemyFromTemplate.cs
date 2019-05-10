2e9659a33fbcda6095ca0eec88e15e22d2d36bf7
manuel Fischer <Manuel.fischeer@gmail.com>
2019-05-11 01:07:27 +0200

    fixed enemy error

--- GameJam19/Assets/Scripts/FPS Template/EnemyFromTemplate.cs
+++ GameJam19/Assets/Scripts/FPS Template/EnemyFromTemplate.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class EnemyFromTemplate : MonoBehaviour {
 
     [SerializeField] bool canFly;
 
