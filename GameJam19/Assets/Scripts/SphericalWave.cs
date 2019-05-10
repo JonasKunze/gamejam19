647d94b5968088b887ced43c816f391dbf01234e
Sebastian Artz <sebastian.artz85@gmail.com>
2019-05-10 21:59:55 +0200

    Init waves

---
+++ GameJam19/Assets/Scripts/SphericalWave.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WavePhysics : MonoBehaviour
+{
+    private double twoSquareHalf;
+    private double springConstant;
+    private double friction;
+
+    private double[] nextAmplitudes;
+    private double[] currentAmplitudes;
+    private double[] velocities;
+
+    private uint xSize;
+    private uint ySize;
+
+    private Mesh mesh;
+    private MeshFilter meshFilter;
+
+    private void Start()
+    {
+        twoSquareHalf = Mathf.Sqrt(2.0f) / 2.0f;
+        xSize = 0;
+        ySize = 0;
+
+        meshFilter = GetComponent<MeshFilter>();
+        mesh = meshFilter.mesh;
+    }
+
+    public void Init(uint xSize, uint ySize, double springConstant, double friction)
+    {
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.springConstant = springConstant;
+        this.friction = friction;
+        var nextAmplitudes = new double[xSize * ySize];
+        var currentAmplitudes = new double[xSize * ySize];
+        var velocities = new double[xSize * ySize];
+        for (int i = 0; i < xSize * ySize; ++i)
+        {
+            currentAmplitudes[i] = 0.0;
+            nextAmplitudes[i] = 0.0;
+            velocities[i] = 0.0;
+        }
+    }
+
+    private double getAmplitude(uint x, uint y)
+    {
+        return nextAmplitudes[x * ySize + y];
+    }
+
+    private void setAmplitude(uint x, uint y, double value)
+    {
+        currentAmplitudes[x * ySize + y] = value;
+    }
+
+    private void update(double deltaT)
+    {
+        for (int x = 1; x < xSize - 1; ++x)
+        {
+            for (int y = 1; y < ySize - 1; ++y)
+            {
+                double force = 0;
+                for (int a = -1; a < 2; ++a)
+                {
+                    for (int b = -1; b < 2; ++b)
+                    {
+                        double difference = currentAmplitudes[(x + a) * ySize + y + b] -
+                                            currentAmplitudes[x * ySize + y];
+                        if (Mathf.Abs(x) + Mathf.Abs(y) == 2)
+                        {
+                            force += twoSquareHalf * difference;
+                        }
+                        else
+                        {
+                            force += difference;
+                        }
+                    }
+                }
+
+                double velocity = velocities[x * ySize + y];
+                force = force * springConstant - velocity * friction;
+                velocity += deltaT * force;
+
+                nextAmplitudes[x * ySize + y] = currentAmplitudes[x * ySize + y] + velocity * deltaT;
+                velocities[x * ySize + y] = velocity;
+            }
+        }
+
+        for (int i = 0; i < xSize * ySize; ++i)
+        {
+            currentAmplitudes[i] = nextAmplitudes[i];
+        }
+    }
+
+/*private void setNodes(Vector<Variant> voxels, int index) {
+	for (int x = 1; x < xSize - 1; ++x) {
+		for (int y = 1; y < ySize -1; ++y) {
+			Spatial* node = (Spatial*)((Node*) voxels[x * ySize + y]);
+			Vector3 translation = node->get_translation();
+			translation[index] = currentAmplitudes[x * ySize + y];
+			node->set_translation(translation);
+		}
+	}
+}*/
+
+    private void setMesh() {
+        mesh.RecalculateNormals();
+        meshFilter.mesh = mesh;
+    }
+}
---
+++ GameJam19/Assets/Scripts/WaveMesh.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMesh : MonoBehaviour
+{
+    private uint xSize = 20;
+    private uint ySize = 20;
+
+    private float scaleX = 0.5f;
+    private float scaleY = 0.5f;
+
+    private Mesh mesh;
+    private MeshFilter meshFilter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        mesh = meshFilter.mesh;
+
+        initMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void initMesh()
+    {
+        Mesh mesh = new Mesh();
+        var vertices = new Vector3[xSize * ySize];
+        var triangles = new int[6 * (xSize - 1) * (ySize - 1)];
+        for (uint x = 0; x < xSize; x++)
+        {
+            for (uint y = 0; y < ySize; y++)
+            {
+                vertices[x * ySize + y] = new Vector3((x - xSize / 2.0f) * scaleX, 0, (y - ySize/2.0f) * scaleY);
+            }
+        }
+
+        int ctr = 0;
+        for (uint x = 0; x < xSize - 1; x++)
+        {
+            for (uint y = 0; y < ySize - 1; y++)
+            {
+                uint triX = x * ySize + y;
+                uint triY = (x + 1) * ySize + (y + 1);
+                uint triZ = (x + 1) * ySize + y;
+                triangles[ctr] = (int) triX;
+                triangles[ctr + 1] = (int) triY;
+                triangles[ctr + 2] = (int) triZ;
+
+                triX = x * ySize + y;
+                triY = x * ySize + (y + 1);
+                triZ = (x + 1) * ySize + (y + 1);
+                triangles[ctr + 3] = (int) triX;
+                triangles[ctr + 4] = (int) triY;
+                triangles[ctr + 5] = (int) triZ;
+
+                ctr += 6;
+            }
+        }
+
+        mesh.Clear();
+        meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.triangles = triangles;
+        meshFilter.mesh.RecalculateBounds();
+        meshFilter.mesh.RecalculateNormals();
+    }
+
+/*    var material = FixedMaterial.new()
+	material.set_parameter(material.PARAM_DIFFUSE, Color(1,0,0,1))
+	var surfTool = SurfaceTool.new()
+	surfTool.set_material(material)
+	surfTool.begin(VS.PRIMITIVE_TRIANGLES)
+	for z in range(sizeZ):
+		for x in range(sizeX):
+			var xCentered = x - sizeX / 2
+			var zCentered = z - sizeZ / 2
+			surfTool.add_normal(Vector3(0,1,0))
+			surfTool.add_vertex(Vector3(xCentered + 0, 0, zCentered + 0))
+			surfTool.add_normal(Vector3(0,1,0))
+			surfTool.add_vertex(Vector3(xCentered + 1, 0, zCentered + 0))
+			surfTool.add_normal(Vector3(0,1,0))
+			surfTool.add_vertex(Vector3(xCentered + 0, 0, zCentered + 1))
+			surfTool.add_normal(Vector3(0,1,0))
+			surfTool.add_vertex(Vector3(xCentered + 1, 0, zCentered + 1))
+			surfTool.add_normal(Vector3(0,1,0))
+			surfTool.add_vertex(Vector3(xCentered + 0, 0, zCentered + 1))
+			surfTool.add_normal(Vector3(0,1,0))
+			surfTool.add_vertex(Vector3(xCentered + 1, 0, zCentered + 0))
+	surfTool.commit(mesh)
+	meshinstance.set_mesh(mesh)
+	meshinstance.create_trimesh_collision()
+	add_child(meshinstance)
+
+/*func initNodes():
+	for x in range(0, sizeX):
+		for z in range(0, sizeZ):
+			var pos = Vector3(x - sizeX/2.0, 0, z - sizeZ/2.0)
+			var box = mapscene.instance()
+			add_child(box)
+			box.set_translation(pos)
+			boxes.append(box)
+	waves.setNodes(boxes, 1)
+
+func _ready():
+	initNodes()
+	waves.init(sizeX, sizeZ, springConstant, friction)
+	self.set_process(true)
+
+var frame = 0
+func _process(deltaT):
+	frame = frame + 1
+	if frame %50 == 0:
+		print(1/deltaT)
+	waves.update(deltaT)
+	
+	# I will build a wall to make games great again
+	#for x in range(0, sizeX/2):
+	#	waves.setAmplitude(x, 10, 0)
+	#	waves.setAmplitude(x, 11, 0)
+	#	waves.setAmplitude(x, 13, 0)
+	
+	for stonePos in stones:
+		waves.setAmplitude(stonePos.x, stonePos.y, 0)
+	
+	if OS.get_unix_time()-lastStompTime < 5:
+		for i in range(-1, 2):
+			waves.setAmplitude(lastStompCenterIndices.x+i, lastStompCenterIndices.y, 0)
+			waves.setAmplitude(lastStompCenterIndices.x, lastStompCenterIndices.y+i, 0)
+	
+	waves.setNodes(boxes, 1)
+
+func stomp(position, amplitudeFactor = 1):
+	get_node("/root/Spatial/SamplePlayer").play("smash")
+	var indexX = int(position.x)+sizeX/2
+	var indexZ = int(position.z)+sizeZ/2
+	
+	for x in range(-4, 5):
+		for z in range(-4, 5):
+			if ((indexX + x < 1) or (indexZ + z < 1) or (indexX + x > sizeX-2) or (indexZ + z > sizeZ-2)):
+				continue
+			var r = sqrt(x*x+z*z)
+			if abs(r-4) < 0.5:
+				waves.setAmplitude(indexX+x, indexZ+z, stomplitude*amplitudeFactor)
+	lastStompTime = OS.get_unix_time()
+	lastStompCenterIndices = Vector2(indexX, indexZ)
+	return Vector3(-sizeX/2+indexX, position.y, -sizeZ/2+indexZ)
+
+func getHeightAt(position):
+	var indexX = int(position.x)+sizeX/2
+	var indexZ = int(position.z)+sizeZ/2
+	
+	return waves.getAmplitude(indexX, indexZ)
+
+func putStone(position):
+	var indexX = int(position.x)+sizeX/2
+	var indexZ = int(position.z)+sizeZ/2
+	
+	stones.append(Vector2(indexX, indexZ))*/
+}
