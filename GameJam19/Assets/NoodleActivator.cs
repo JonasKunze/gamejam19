af46788261b83c9925add219cfecfcf24c855871
Jonas Kunze <kunze.jonas@gmail.com>
2019-05-11 21:38:04 +0200

    Workaround for noode: don't add to weapons root gameobject

---
+++ GameJam19/Assets/NoodleActivator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class NoodleActivator : MonoBehaviour
+{
+    public GameObject noodleWeapon;
+    public GameObject model;
+
+    void Update()
+    {
+        model.SetActive(noodleWeapon.gameObject.GetActive());  
+    }
+}
