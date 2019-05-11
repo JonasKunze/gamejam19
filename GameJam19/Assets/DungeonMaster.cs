6c024f900b401932c8bca555d60f5b0b08fbef95
Daniel <dwilhelm@mail.de>
2019-05-11 18:54:30 +0200

    stop wave adjusted

--- GameJam19/Assets/DungeonMaster.cs
+++ GameJam19/Assets/DungeonMaster.cs
@@ -19,6 +19,7 @@ public class DungeonMaster : MonoBehaviour
 
     [SerializeField] private uint NumberOfWaves;
     [SerializeField] private EnemySpawner[] EnemySpawners;
+    private int currentActiveSpawners;
     [SerializeField] private Shop shop;
     private uint currentWave;
     private GameState gameState;
@@ -35,6 +36,7 @@ public class DungeonMaster : MonoBehaviour
         gameState = GameState.Start;
         currentWave = 0;
         currentNumberOfEnemies = 0;
+        currentActiveSpawners = 0;
     }
 
     IEnumerator CORStartNewWave(uint TimeStart)
@@ -71,13 +73,25 @@ public class DungeonMaster : MonoBehaviour
             {
                 spawner.StartSpawning(TimeWaveDuration);
             }
+
+            currentActiveSpawners = EnemySpawners.Length;
+
         }
     }
 
     private void EndCurrentWave()
     {
+        currentNumberOfEnemies = 0;
+        currentActiveSpawners = 0;
         gameState = GameState.Idle;
         Shop.OpenShopCanvas();
+    }
+
+    public bool IsWaveDone()
+    {
+        if (gameState != GameState.Wave) return true;
+
+        return ((currentActiveSpawners <= 0) && (currentNumberOfEnemies <= 0));
     }
 
     public void RegisterEnemyBorn()
@@ -88,7 +102,16 @@ public class DungeonMaster : MonoBehaviour
     public void RegisterEnemyKilled()
     {
         --currentNumberOfEnemies;
-        if (currentNumberOfEnemies <= 0)
+        if (IsWaveDone())
+        {
+            EndCurrentWave();
+            StartCoroutine(CORStartNewWave(TimePause));
+        }
+    }
+    public void RegisterSpawnerIsDone()
+    {
+        --currentActiveSpawners;
+        if (IsWaveDone())
         {
             EndCurrentWave();
             StartCoroutine(CORStartNewWave(TimePause));
