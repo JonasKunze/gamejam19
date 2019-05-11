40d4dcbf75a4af6b272aa5eb78634989a4f1ca44
Sebastian Artz <sebastian.artz85@gmail.com>
2019-05-11 20:00:05 +0200

    pumpgun

---
+++ GameJam19/Assets/Scripts/Pumpgun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpgun : MonoBehaviour
+{
+
+    public GameObject waterBeam;
+    public GameObject spawnPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shoot()
+    {
+        GameObject obj = Instantiate(waterBeam, spawnPosition.transform.position, spawnPosition.transform.rotation);
+        ParticleSystem ps = obj.GetComponent<ParticleSystem>();
+        ps.Play();
+    }
+}
