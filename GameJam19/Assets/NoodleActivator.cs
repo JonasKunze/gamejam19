18b5c262e7b28e3a536cf2d660837f6f20226f37
Arrigo Paterno <arrigo.paterno@flyingshapes.com>
2019-05-12 13:09:03 +0200

    Fixed noodle bug

--- GameJam19/Assets/NoodleActivator.cs
+++ GameJam19/Assets/NoodleActivator.cs
@@ -8,8 +8,69 @@ public class NoodleActivator : MonoBehaviour
     public GameObject noodleWeapon;
     public GameObject model;
 
+    private IEnumerator coroutine;
+
+    private Quaternion startRotation;
+    public Vector3 targetRotation;
+    
+    
+    private void Start()
+    {
+        startRotation = model.transform.localRotation;
+    }
+
     void Update()
     {
         model.SetActive(noodleWeapon.gameObject.GetActive());  
+    }
+
+    public void StartAnimation()
+    {
+        if (coroutine != null)
+            StopCoroutine(coroutine);
+
+        coroutine = null;
+
+        coroutine = Animate();
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Animate()
+    {
+        model.transform.localRotation = startRotation;
+
+        float startTime = 0f, animTime = .25f;
+
+        float percent = 0f;
+
+        while (percent < 1)
+        {
+            percent = startTime / animTime;
+            percent = Mathf.Clamp01(percent);
+
+            model.transform.localRotation =
+                Quaternion.Lerp(model.transform.localRotation, Quaternion.Euler(targetRotation), percent);
+            
+            startTime += Time.deltaTime;
+            
+            yield return null;
+        }
+
+        startTime = percent = 0;
+        
+        while (percent < 1)
+        {
+            percent = startTime / animTime;
+            percent = Mathf.Clamp01(percent);
+
+            model.transform.localRotation =
+                Quaternion.Lerp(model.transform.localRotation, Quaternion.identity, percent);
+            
+            startTime += Time.deltaTime;
+            
+            yield return null;
+        }
+        
+        model.transform.localRotation  = Quaternion.identity;
     }
 }
